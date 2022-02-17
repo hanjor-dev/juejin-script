6af06d1bf9e6f3c38c3e5e1af8c8f992a396b16a
韩俊杰Junjie Han <junjie.han@minfound.com>
2022-02-17 17:15:23 +0800

    init

---
+++ src/main/java/com/hanjor/juejinscript/JuejinScriptApplication.java
@@ -0,0 +1,13 @@
+package com.hanjor.juejinscript;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuejinScriptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuejinScriptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanjor/juejinscript/JuejinScriptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanjor.juejinscript;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuejinScriptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
